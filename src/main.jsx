5a5838eff4a8c4da4d12f9ce84d415b5d71b8a82
21r01a7350 <138276540+21r01a7350@users.noreply.github.com>
2024-07-04 12:22:40 +0530

    initialized vite+react application and installed the deps

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {ChakraProvider,extendTheme } from '@chakra-ui/react'
+import App from './App.jsx'
+import './index.css'
+Export const theme=extendTheme({
+  fonts :{
+    body :"'Josefin Sans,sans-serif"
+  },
+});
+ReactDOM.createRoot(document.getElementById('root')).render(
+ <ChakraProvider theme={theme}>
+   <App />
+ </ChakraProvider>  
+)
